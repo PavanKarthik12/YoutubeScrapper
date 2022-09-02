a20e1442e32c2ffdd7731e35567d7b1a0af16ddc
PavanKarthik12 <40707193+PavanKarthik12@users.noreply.github.com>
2022-09-03 00:02:31 +0530

    Update titles.py

--- titles.py
+++ titles.py
@@ -117,3 +117,4 @@ def search_download(search_term,number_images,target_path='./images'):
         insert_query='''INSERT INTO {0} VALUES('{1}','{2}','{3}','{4}','{5}')'''.format(channel_name,counter,title,link,likes,comments_count)
         counter+=1
         snowflake_connnect(insert_query)
+        return "Data is excuted"
