16aa6fe7691221bf8adb70b334b6d3ef73aa85bb
PavanKarthik12 <40707193+PavanKarthik12@users.noreply.github.com>
2022-09-04 01:13:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,12 @@ def index():
     if request.method == 'POST':
         try:
             channel_link=request.form['content']
-            print(search_download(channel_link,1))
+            print(search_download(channel_link,10))
             channel_title="".join(i for i in channel_link.split("/")[4] if i.isalnum())
             counter=0
+            print(counter)
             for each_row in range(len(videos_links)):
+                print(each_row)
                 if each_row in videos_title:
                     title=videos_title[each_row]
                 else:
@@ -42,10 +44,11 @@ def index():
                 insert_query='''INSERT INTO {0} VALUES('{1}','{2}','{3}','{4}','{5}')'''.format(channel_title,counter,title,link,likes,comments_count)
                 snowflake_connnect(insert_query)
                 counter+=1
+            print(channel_title)
             select_query=''' select * from {0}'''.format(channel_title)
             query_result=snowflake_connnect(select_query,'select')    
-        except:
-            print("Exception occured while collectiong data")
+        except Exception as exc:
+            print(exc)
             return render_template('index.html')
         else:
             return render_template('results.html',query_results=query_result)
