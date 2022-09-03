e24df5a623091da0570d98eecc28981373b4c6dc
PavanKarthik12 <40707193+PavanKarthik12@users.noreply.github.com>
2022-09-03 08:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import titles
 from titles import search_download,snowflake_connnect
+from titles import videos_comments_count,videos_likes,videos_links,videos_title
 app = Flask(__name__)
 @app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
@@ -17,15 +18,37 @@ def homePage():
 def index():
     if request.method == 'POST':
         try:
-           channel_link=request.form['content']
-           print(search_download(channel_link,10))
-           channel_title="".join(i for i in channel_link.split("/")[4] if i.isalnum())
-           select_query=''' select * from {0}'''.format(channel_title)
-           #query_result=snowflake_connnect(select_query,'select')
+            channel_link=request.form['content']
+            print(search_download(channel_link,2))
+            channel_title="".join(i for i in channel_link.split("/")[4] if i.isalnum())
+            counter=0
+            for each_row in range(len(videos_links)):
+                if each_row in videos_title:
+                    title=videos_title[each_row]
+                else:
+                    title=""
+                if each_row in videos_links:
+                    link=videos_links[each_row]
+                else:
+                    link=""
+                if each_row in videos_likes:
+                    likes=videos_likes[each_row]
+                else:
+                    likes=""
+                if each_row in videos_comments_count:
+                    comments_count=videos_comments_count[each_row]
+                else:
+                    comments_count=""
+                insert_query='''INSERT INTO {0} VALUES('{1}','{2}','{3}','{4}','{5}')'''.format(channel_title,counter,title,link,likes,comments_count)
+                snowflake_connnect(insert_query)
+                counter+=1
+            select_query=''' select * from {0}'''.format(channel_title)
+            query_result=snowflake_connnect(select_query,'select')    
         except:
             print("Exception occured while collectiong data")
+            return render_template('index.html')
         else:
-            return render_template('index.html',query_results=query_result)
+            return render_template('results.html',query_results=query_result)
     else:
         return render_template('index.html')
 
